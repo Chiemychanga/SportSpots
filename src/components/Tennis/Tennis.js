be2fc9e72e772d3fa085b234cf3d68e6446e9f5a
unknown <leedanleo.91@gmail.com>
2020-02-16 16:08:06 -0800

    Sucessfully routed the main page to a tennis page. On Tennis branch.

---
+++ src/components/Tennis/Tennis.js
@@ -0,0 +1,10 @@
+import React from 'react';
+// import { Navbar, Nav, NavItem, MenuItem, NavDropdown } from 'react-bootstrap';
+
+export default class MondayTennis extends React.Component {
+    render() {
+        return (
+            <h1>Tennis Spots</h1>
+        )
+    }
+}
