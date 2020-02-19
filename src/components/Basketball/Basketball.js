47ce6a149212d8c75024cac0d6703ff80e471fd0
Joseph Jesse <jjesse@pdx.edu>
2020-02-18 22:58:14 -0800

    More work to basketball component

--- src/components/Basketball/Basketball.js
+++ src/components/Basketball/Basketball.js
@@ -1,24 +1,104 @@
-import React from 'react';
-import './App.css';
-import Footer from "./components/Footer";
-import Header from "./components/Header";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {OpenGymInst} from "./OpenGymInst";
+import React from "react";
+import "../css/content.css";
+import Footer from "../Footer.js";
+import Header from "../Header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { OpenGymInst } from "../OpenGymInst";
+import { OpenGym } from "../../OpenGymInst";
 
-let openGyms = [
-    new OpenGymInst("Mt. Scott Community Center", "5530 Southeast 72nd Avenue", "Sunday", "2:30-4:30 PM", "$0-$6.25", "All Ages"),
-    new OpenGymInst("Mt. Scott Community Center", "5530 Southeast 72nd Avenue", "Tuesday", "7:20-9:20 PM", "$0-$6.25", "Ages 18+ (ID Required)"),
-    new OpenGymInst("Mt. Scott Community Center", "5530 Southeast 72nd Avenue", "Thursday", "7:20-9:20 PM", "$0-$6.25", "Ages 18+ (ID Required)"),
-    new OpenGymInst("Mt. Scott Community Center", "5530 Southeast 72nd Avenue", "Saturday", "4:45-6:45 PM", "$0-$6.25", "Ages 30+ (ID Required)"),
-    new OpenGymInst("Mt. Scott Community Center", "5530 Southeast 72nd Avenue", "Sunday", "4:45-6:45 PM", "$0-$6.25", "Ages 30+ (ID Required)")
-]
 
-export class Basketball extends React.Component {
-    render() {
-        return (
-            <div>
-                <OpenGymInst gymName="Mt. Scott Community Center" address="5530 Southeast 72nd Avenue" time="2:30-4:30 PM" cost="$0-$6.25" description="All Ages." />
-            </div>
-        )
-    }
+const openGyms = [
+  new OpenGymInst(
+    null,
+    "Mt. Scott Community Center",
+    "5530 Southeast 72nd Avenue",
+    null,
+    "Basketball",
+    "Sunday",
+    "2:30-4:30 PM",
+    "$0-$6.25",
+    "All Ages",
+    "https://www.portlandoregon.gov/parks/article/634572",
+    null,
+    null,
+    null,
+    null
+  ),
+  new OpenGymInst(
+    null,
+    "Mt. Scott Community Center",
+    "5530 Southeast 72nd Avenue",
+    null,
+    "Basketball",
+    "Tuesday",
+    "7:20-9:20 PM",
+    "$0-$6.25",
+    "Ages 18+ (ID Required)",
+    "https://www.portlandoregon.gov/parks/article/634572",
+    null,
+    null,
+    null,
+    null
+  ),
+  new OpenGymInst(
+    null,
+    "Mt. Scott Community Center",
+    "5530 Southeast 72nd Avenue",
+    null,
+    "Basketball",
+    "Thursday",
+    "7:20-9:20 PM",
+    "$0-$6.25",
+    "Ages 18+ (ID Required)",
+    "https://www.portlandoregon.gov/parks/article/634572",
+    null,
+    null,
+    null,
+    null
+  ),
+  new OpenGymInst(
+    null,
+    "Mt. Scott Community Center",
+    "5530 Southeast 72nd Avenue",
+    null,
+    "Basketball",
+    "Saturday",
+    "4:45-6:45 PM",
+    "$0-$6.25",
+    "Ages 30+ (ID Required)",
+    "https://www.portlandoregon.gov/parks/article/634572",
+    null,
+    null,
+    null,
+    null
+  ),
+  new OpenGymInst(
+    null,
+    "Mt. Scott Community Center",
+    "5530 Southeast 72nd Avenue",
+    null,
+    "Basketball",
+    "Sunday",
+    "4:45-6:45 PM",
+    "$0-$6.25",
+    "Ages 30+ (ID Required)",
+    "https://www.portlandoregon.gov/parks/article/634572",
+    null,
+    null,
+    null,
+    null
+  )
+];
+
+
+export default class Basketball extends React.Component {
+  render() {
+    return (
+      <div>
+        {openGyms.map(function(gym) {
+          return <OpenGym gymName={gym.getGymName()} address={gym.getAddress()} time={gym.getTime()} cost={gym.getCost()} description={gym.getLevelDescription()}/>
+        })}
+      </div>
+    );
+  }
 }
