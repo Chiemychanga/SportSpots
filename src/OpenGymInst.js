1188ef72f8b911f49f7fe2b2e3bafd433a912834
Joseph Jesse <jjesse@pdx.edu>
2020-02-19 21:45:49 -0800

    Basketball styling

--- src/OpenGymInst.js
+++ src/OpenGymInst.js
@@ -5,9 +5,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export class OpenGym extends React.Component {
     render() {
         return (
-            <div className = "container align-center">
-                <h2>{this.props.gymName}</h2>
-                <h3>{this.props.address}</h3>
+            <div className="container align-center">
+                <h3>
+                    {this.props.gymName}
+                </h3>
+                <br />
+                <br />
+                <h5>{this.props.address}</h5>
+                <br />
                 <p>Time: {this.props.time}
                 <br />
                 Cost: {this.props.cost}
--- src/components/Basketball/Basketball.js
+++ src/components/Basketball/Basketball.js
@@ -15,8 +15,8 @@ const openGyms = [
     null,
     "Basketball",
     "Sunday",
-    "2:30-4:30 PM",
-    "$0-$6.25",
+    "2:30 - 4:30 PM",
+    "$0 - $6.25",
     "All Ages",
     "https://www.portlandoregon.gov/parks/article/634572",
     null,
@@ -31,8 +31,8 @@ const openGyms = [
     null,
     "Basketball",
     "Tuesday",
-    "7:20-9:20 PM",
-    "$0-$6.25",
+    "7:20 - 9:20 PM",
+    "$0 - $6.25",
     "Ages 18+ (ID Required)",
     "https://www.portlandoregon.gov/parks/article/634572",
     null,
@@ -47,8 +47,8 @@ const openGyms = [
     null,
     "Basketball",
     "Thursday",
-    "7:20-9:20 PM",
-    "$0-$6.25",
+    "7:20 - 9:20 PM",
+    "$0 - $6.25",
     "Ages 18+ (ID Required)",
     "https://www.portlandoregon.gov/parks/article/634572",
     null,
@@ -63,8 +63,8 @@ const openGyms = [
     null,
     "Basketball",
     "Saturday",
-    "4:45-6:45 PM",
-    "$0-$6.25",
+    "4:45 - 6:45 PM",
+    "$0 - $6.25",
     "Ages 30+ (ID Required)",
     "https://www.portlandoregon.gov/parks/article/634572",
     null,
@@ -79,8 +79,8 @@ const openGyms = [
     null,
     "Basketball",
     "Sunday",
-    "4:45-6:45 PM",
-    "$0-$6.25",
+    "4:45 - 6:45 PM",
+    "$0 - $6.25",
     "Ages 30+ (ID Required)",
     "https://www.portlandoregon.gov/parks/article/634572",
     null,
@@ -95,21 +95,59 @@ export default class Basketball extends React.Component {
     return (
       <div>
         <br />
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
+                aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div id="daysOfWeekNav" className="navbar-nav">
+                    <a className="nav-item nav-link" href="#">Sunday</a>
+                    <a className="nav-item nav-link" href="#">Monday</a>
+                    <a className="nav-item nav-link" href="#">Tuesday</a>
+                    <a className="nav-item nav-link" href="#">Wednesday</a>
+                    <a className="nav-item nav-link" href="#">Thursday</a>
+                    <a className="nav-item nav-link" href="#">Friday</a>
+                    <a className="nav-item nav-link" href="#">Saturday</a>
+                </div>
+            </div>
+        </nav>
+        <br />
         <h1 className="align-center">Basketball</h1>
         <br />
         <img src={basketballPicture} alt="Athletes playing basketball" />
         <br />
         <br />
         <br />
+        <br />
+        <br />
         {openGyms.map(function(gym) {
           return (
-            <OpenGym
-              gymName={gym.getGymName()}
-              address={gym.getAddress()}
-              time={gym.getTime()}
-              cost={gym.getCost()}
-              description={gym.getLevelDescription()}
-            />
+            <div className="container align-center {gym.getDayOfWeek()}">
+                <h3>
+                    {gym.getGymName()}
+                </h3>
+                <h5>{gym.getAddress()}</h5>
+                <br />
+                <p><b>Time:</b> {gym.getTime()}
+                <br />
+                <b>Cost:</b> {gym.getCost()}
+                <br />
+                <b>Description:</b> {gym.getLevelDescription()}
+                <br />
+                <br />
+                <a href="{gym.getWebsite()}">{gym.getWebsite()}</a>
+                </p>
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+                <br />
+            </div>
           );
         })}
         <SportFooter sport="Basketball"/>
