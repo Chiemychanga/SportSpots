89c8f915be15389973d529b64b4a11263dd6e616
Joseph Jesse <jjesse@pdx.edu>
2020-02-22 15:50:15 -0800

    Refractored to use gym prop and improved img tag

--- src/OpenGymInst.js
+++ src/OpenGymInst.js
@@ -5,19 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export class OpenGym extends React.Component {
     render() {
         return (
-            <div className="container align-center">
-                <h3>
-                    {this.props.gymName}
-                </h3>
+            <div>
+              <h3>{this.props.gym.getGymName()}</h3>
+              <h5>{this.props.gym.getAddress()}</h5>
+              <br />
+              <p>
+                <b>Time:</b> {this.props.gym.getTime()}
+                <br />
+                <b>Cost:</b> {this.props.gym.getCost()}
+                <br />
+                <b>Description:</b> {this.props.gym.getLevelDescription()}
                 <br />
                 <br />
-                <h5>{this.props.address}</h5>
-                <br />
-                <p>Time: {this.props.time}
-                <br />
-                Cost: {this.props.cost}
-                <br />
-                Description: {this.props.description}</p>
+                <a rel="noopener noreferrer" href={this.props.gym.getWebsite()} target="_blank">{this.props.gym.getWebsite()}</a>
+              </p>
+              <br />
+              <br />
+              <br />
             </div>
         );
     }
