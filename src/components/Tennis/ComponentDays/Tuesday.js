e55f7ec33c1930cb01d2f00535b41b50babb9e06
unknown <leedanleo.91@gmail.com>
2020-02-23 22:04:53 -0800

    Changed code to match and use OpenGymInst.

---
+++ src/components/Tennis/ComponentDays/Tuesday.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { OpenGymInst } from "../../OpenGymInst";
+import PortlandTennisCenter from "../images/PortlandTennisCenter.png"; 
+import StJohnsRacquetCenter from "../images/StJohnsRacquetCenter.jpg"; 
+import LakeOswegoTennisCenter from "../images/LakeOswegoTennisCenter.png"; 
+
+export default class Monday extends React.Component {
+    render() {
+        const gym1 = new OpenGymInst(PortlandTennisCenter, "Portland Tennis Center", "324 NE 12th Avenue, Portland, Oregon 97232", "https://goo.gl/maps/pD42T9Nez3XxQejD7", "Tennis", "Monday through Friday", "5:30am to 11:00pm", "$16.00 per Court for 75 minutes", "Singles/Doubles", "http://www.thprd.org/facilities/recreation/conestoga/schedule/dropinsports/#dropin", "Reservations open 3 days in advance at 8:30am.", "8", "Indoor", "None");
+        const gym2 = new OpenGymInst(StJohnsRacquetCenter, "St. Johns Racquet Center", "7519 N Burlington Ave, Portland, OR 97203", "https://goo.gl/maps/d7WVkBReSGAJVeb4A", "Tennis", "Monday and Thursday", "8:00am to 2:00pm and 6:00pm to 10:00pm", "$30.00 per Court 75 minutes", "Singles/Doubles", "https://www.stjohnsracquetcenter.org/", "Courts can be booked up to 7 days in advance.", "8", "Indoor", "None");
+        const gym3 = new OpenGymInst(LakeOswegoTennisCenter, "Lake Oswego Tennis Center", "2900 SW Diane Dr, Lake Oswego, OR 97035", "https://goo.gl/maps/3QUUnJDZjpMpqtF17", "Tennis", "Monday and Thursday", "6:00am to 10:00pm", "$22.00 per Court 60 minutes", "Singles/Doubles", "https://www.ci.oswego.or.us/parksrec/indoor-tennis-center", "Courts can be booked up to 7 days in advance.", "8", "Indoor", "None");
+        const gyms = [gym1,gym2,gym3];
+        return (
+            <div className="gym-spots">
+                <h1>Tennis Spots Tuesday</h1>
+
+                {gyms.map(function (gym) {
+                    return (
+                        <div className="gym-info">
+                        <img src={gym.image} alt={gym.gymName}></img>
+                        <h2>{gym.gymName}</h2>
+                        <li>Address: <a href={gym.googleAddressLink}>{gym.address}</a></li>
+                        <li>Day(s) of the week: {gym.dayOfWeek}</li>
+                        <li>Time: {gym.time}</li>
+                        <li>Cost: {gym.cost}</li>
+                        <li>Level: {gym.levelDescription}</li>
+                        <li>Website: <a href={gym.website}>{gym.website}</a></li>
+                        <li>Description: {gym.description}</li>
+                        <li>Number of Courts: {gym.numberOfCourts}</li>
+                        <li>Court Type: {gym.courtType}</li>
+                        <li>Minimum Age: {gym.minimumAge}</li>
+                        </div>
+                    )
+                })}
+
+            </div>
+        )
+    }
+}
--- src/components/Tennis/Tennis.js
+++ src/components/Tennis/Tennis.js
@@ -1,30 +1,120 @@
 import React from 'react';
 import "./css/tennis.css";
-import gym1 from "./images/portlandTennisCenter.png";
-import gym2 from "./images/lakeOswegoTennisCenter.png"
 import SportFooter from "../SportFooter.js";
+import SportFooter from "../SportFooter.js";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class MondayTennis extends React.Component {
+export default class Tennis extends React.Component {
+    state = {
+        dayOfWeek: ""
+    };
+
+    getDisplay() {
+        if(this.state.dayOfWeek === "Sunday") {
+          return <Sunday />
+        } else if(this.state.dayOfWeek === "Saturday") {
+          return <Saturday />
+        } else if(this.state.dayOfWeek === "Monday") {
+          return <Monday />
+        } else if(this.state.dayOfWeek === "Tuesday") {
+          return <Tuesday />
+        } else if(this.state.dayOfWeek === "Wednesday") {
+          return <Wednesday />
+        } else if(this.state.dayOfWeek === "Thursday") {
+          return <Thursday />
+        } else {
+          return <Friday />
+        }
+    }
     render() {
+        if (this.state.dayOfWeek === "") {
+          var d = new Date();
+          var weekday = new Array(7);
+          weekday[0] = "Sunday";
+          weekday[1] = "Monday";
+          weekday[2] = "Tuesday";
+          weekday[3] = "Wednesday";
+          weekday[4] = "Thursday";
+          weekday[5] = "Friday";
+          weekday[6] = "Saturday";
+    
+          this.setState({dayOfWeek: weekday[d.getDay()]});
+        }
         return (
-            <div class="tennis">
-                <h1>Tennis Spots</h1>
-                <div class="container">
-                    <div class="box">                    
-                        <img src={gym1} alt="gym1"/><br></br>
-                        Portland Tennis Center <br></br>
-                        324 NE 12th Ave, Portland, OR 97232<br></br>
-                        (503) 823-3189
-                    </div>
-                    <div class="box">                  
-                        <img src={gym2} alt="gym2"/><br></br>
-                        Lake Oswego Tennis Center <br></br>
-                        2900 SW Diane Dr, Lake Oswego, OR 97035<br></br>
-                        (503) 635-5550
-                    </div>
-                </div>
-                <SportFooter sport="Tennis"/>
+        <div>
+          <br />
+          <nav className="navbar navbar-expand-lg navbar-light">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div id="daysOfWeekNav" className="navbar-nav">
+                <a
+                  className="nav-item nav-link SundayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Sunday"})}}
+                  href="#"
+                >
+                  Sunday
+                </a>
+                <a
+                  className="nav-item nav-link MondayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Monday"})}}
+                  href="#"
+                >
+                  Monday
+                </a>
+                <a
+                  className="nav-item nav-link TuesdayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Tuesday"})}}
+                  href="#"
+                >
+                  Tuesday
+                </a>
+                <a
+                  className="nav-item nav-link WednesdayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Wednesday"})}}
+                  href="#"
+                >
+                  Wednesday
+                </a>
+                <a
+                  className="nav-item nav-link ThursdayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Thursday"})}}
+                  href="#"
+                >
+                  Thursday
+                </a>
+                <a
+                  className="nav-item nav-link FridayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Friday"})}}
+                  href="#"
+                >
+                  Friday
+                </a>
+                <a
+                  className="nav-item nav-link SaturdayLink"
+                  onClick={(e) => {this.setState({dayOfWeek: "Saturday"})}}
+                  href="#"
+                >
+                  Saturday
+                </a>
+              </div>
             </div>
-        )
+          </nav>
+          <br />
+          <h1 className="align-center">{this.state.dayOfWeek} Basketball</h1>
+        
+          {this.getDisplay()}
+          <SportFooter sport="Tennis" />
+        </div>
+      );
     }
 }
