9636cf378a14c2c3158306fd068096c6f24b6965
unknown <leedanleo.91@gmail.com>
2020-02-24 18:43:18 -0800

    Fixed error quick fix.

--- src/components/Tennis/ComponentDays/Friday.js
+++ src/components/Tennis/ComponentDays/Friday.js
@@ -2,8 +2,7 @@ import React from 'react';
 import { OpenGymInst } from "../../OpenGymInst";
 import PortlandTennisCenter from "../images/PortlandTennisCenter.png"; 
 import StJohnsRacquetCenter from "../images/StJohnsRacquetCenter.jpg"; 
-import LakeOswegoTennisCenter from "../images/lakeOswegoTennisCenter.png"; 
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import LakeOswegoTennisCenter from "../images/LakeOswegoTennisCenter.png"; 
 
 export default class Monday extends React.Component {
     render() {
