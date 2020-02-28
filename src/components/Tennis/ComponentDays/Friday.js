6db5f6fd9374d6b470ea3983dbd459b7e32d8f8c
Joseph Jesse <jjesse@pdx.edu>
2020-02-27 20:56:45 -0800

    Fixed typos related to path of image files

--- src/components/Tennis/ComponentDays/Friday.js
+++ src/components/Tennis/ComponentDays/Friday.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { OpenGymInst } from "../../OpenGymInst";
-import PortlandTennisCenter from "../images/PortlandTennisCenter.png"; 
+import PortlandTennisCenter from "../images/portlandTennisCenter.png"; 
 import StJohnsRacquetCenter from "../images/StJohnsRacquetCenter.jpg"; 
-import LakeOswegoTennisCenter from "../images/LakeOswegoTennisCenter.png"; 
+import LakeOswegoTennisCenter from "../images/lakeOswegoTennisCenter.png"; 
 
 export default class Monday extends React.Component {
     render() {
