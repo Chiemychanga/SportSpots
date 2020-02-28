85cefc7390b523eac6a605d3a68b41120706ceea
Joseph Jesse <jjesse@pdx.edu>
2020-02-27 22:05:42 -0800

    displayGyms component - filter/display gyms by day

---
+++ src/components/Basketball/displayGyms.js
@@ -0,0 +1,18 @@
+import React from "react";
+import gymsArray from "./gymsArray";
+import { OpenGym } from "../../OpenGymInst";
+
+export default class DisplayGyms extends React.Component {
+  render() {
+    const newGymArray = gymsArray.filter(
+      gym => gym.dayOfWeek === this.props.dayOfWeek
+    );
+    return (
+      <div>
+        {newGymArray.map(function(gym) {
+          return <OpenGym gym={gym} />;
+        })}
+      </div>
+    );
+  }
+}
