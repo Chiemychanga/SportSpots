cb82f75618766bd513f0fda601313f898a2f080a
Joseph Jesse <jjesse@pdx.edu>
2020-02-29 10:57:16 -0800

    Changed order of gym data rendering

--- src/components/Basketball/OpenGymInst.js
+++ src/components/Basketball/OpenGymInst.js
@@ -4,12 +4,15 @@ export class OpenGym extends React.Component {
   render() {
     return (
       <div className="gym-info">
-        <img src={this.props.gym.getImage()} alt={this.props.gym.getGymName()}></img>
         <h2>{this.props.gym.getGymName()}</h2>
+        <br />
+        <img src={this.props.gym.getImage()} alt={this.props.gym.getGymName()}></img>
+        <br />
+        <br />
         <li><b>Address:</b> <a href={this.props.gym.getGoogleAddressLink()}>{this.props.gym.getAddress()}</a></li>
         <li><b>Time:</b> {this.props.gym.getTime()}</li>
         <li><b>Cost:</b> {this.props.gym.getCost()}</li>
-        <li><b>Level:</b> {this.props.gym.getLevelDescription()}</li>
+        <li><b>Description:</b> {this.props.gym.getLevelDescription()}</li>
         <li><b>Website:</b> <a href={this.props.gym.getWebsite()}>{this.props.gym.getWebsite()}</a></li>
       </div>
     );
