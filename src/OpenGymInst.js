870f6c54914d984f81c3f188768b13225ed21e08
Joseph Jesse <jjesse@pdx.edu>
2020-02-28 17:13:41 -0800

    Removed old code

--- src/OpenGymInst.js
+++ src/OpenGymInst.js
@@ -3,26 +3,6 @@ import React from "react";
 export class OpenGym extends React.Component {
   render() {
     return (
-      /*
-            <div>
-              <h3>{this.props.gym.getGymName()}</h3>
-              <h5>{this.props.gym.getAddress()}</h5>
-              <br />
-              <p>
-                <b>Time:</b> {this.props.gym.getTime()}
-                <br />
-                <b>Cost:</b> {this.props.gym.getCost()}
-                <br />
-                <b>Description:</b> {this.props.gym.getLevelDescription()}
-                <br />
-                <br />
-                <a rel="noopener noreferrer" href={this.props.gym.getWebsite()} target="_blank">{this.props.gym.getWebsite()}</a>
-              </p>
-              <br />
-              <br />
-              <br />
-            </div>
-            */
       <div className="gym-info">
         <img src={this.props.gym.getImage()} alt={this.props.gym.getGymName()}></img>
         <h2>{this.props.gym.getGymName()}</h2>
