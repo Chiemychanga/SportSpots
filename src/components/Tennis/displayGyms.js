6b8c8dfa6e4e29961a87805436df685e62084432
Daniel Lee <leedanleo.91@gmail.com>
2020-02-29 23:12:27 -0800

    Currently the sport sites are displaying properly on the tennis end, the method of including sport sites are based on if they have the day displayed.

--- src/components/Tennis/displayGyms.js
+++ src/components/Tennis/displayGyms.js
@@ -1,18 +1,18 @@
-import React from "react"; 
-import gymsArray from "./gymsArray"; //standard arrays that store all the gyms based on days. A filter is used to determine which gym appears on what day
-import { OpenGym } from "./OpenGymInst"; // uses jsx to display and get the gym information.
+import React from "react";
+import gymsArray from "./gymsArray";
+import { OpenGym } from "./OpenGymInst";
 
-export default class DisplayGyms extends React.Component { //new class
-    render() {
-      const newGymArray = gymsArray.filter( //creates a new array from the array to only show gyms based on what day of the week is set.
-        gym => gym.dayOfWeek === this.props.dayOfWeek
-      );
-      return (
-        <div>
-          {newGymArray.map(function(gym) { //returns the new array of gyms
-            return <OpenGym gym={gym} />; //uses opengym component class
-          })}
-        </div>
-      );
-    }
+export default class DisplayGyms extends React.Component {
+  render() {
+    const newGymArray = gymsArray.filter( 
+      gym => gym.dayOfWeek.includes(this.props.dayOfWeek)
+    );
+    return (
+      <div>
+        {newGymArray.map(function(gym, i) {
+          return <OpenGym gym={gym} key={i}/>;
+        })}
+      </div>
+    );
   }
+}
