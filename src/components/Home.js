89442d79d046f867c7ddba910e7a317b8b041b2a
unknown <leedanleo.91@gmail.com>
2020-03-01 22:41:11 -0800

    Fixed the home page to correctly link to therespective pages, and replaced to clip art images.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./css/home.css";
-import portal1 from "./images/volleyball.png";
-import portal2 from "./images/basketball.png";
-import portal3 from "./images/tennis.png";
-import portal4 from "./images/soccer.png";
+import portal1 from "./images/volleyballPortal.png";
+import portal2 from "./images/basketballPortal.png";
+import portal3 from "./images/tennisPortal.png";
+import portal4 from "./images/soccerPortal.png";
 import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
     render() {
@@ -13,13 +14,13 @@ export default class Home extends React.Component {
                 <h1>Home</h1>
                 <div className="container">
                     <div className="box">                    
-                        <img src={portal1} alt="volleyball-portal"/><br></br>
+                        <Link to="/Volleyball"><img src={portal1} alt="volleyball-portal"/><br></br></Link>
                     </div>
                     <div className="box">                  
-                        <img src={portal2} alt="basketball-portal"/><br></br>
+                        <Link to="/Basketball"><img src={portal2} alt="basketball-portal"/><br></br></Link>
                     </div>
-                    <div className="box">                  
-                        <img src={portal3} alt="tennis-portal"/><br></br>
+                    <div className="box">   
+                        <Link to="/Tennis"><img src={portal3} alt="tennis-portal"/><br></br></Link>               
                     </div>
                     <div className="box">                  
                         <img src={portal4} alt="soccer-portal"/><br></br>
