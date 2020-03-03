52a52e4fd6c81546f81fa467596f1810b0fb9ca2
Joseph Jesse <jjesse@pdx.edu>
2020-03-02 17:32:45 -0800

    Refactoring of tennis to use GMap

--- src/components/Tennis/Tennis.js
+++ src/components/Tennis/Tennis.js
@@ -4,7 +4,8 @@ import SportFooter from "../SportFooter.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import DayOfWeekNav from "../DayOfWeekNav";
 import DisplayGyms from "./displayGyms";
-import GMAP from "./GMap.js";
+import GMAP from "../GMap";
+import gymsArray from "./gymsArray";
 
 export default class Tennis extends React.Component {
     constructor() {
@@ -27,7 +28,7 @@ export default class Tennis extends React.Component {
             <div>
                 <DayOfWeekNav handleClick={this.handleClick} />
                 <h1 className="align-center">{this.state.dayOfWeek} Tennis</h1>
-                <GMAP dayOfWeek={this.state.dayOfWeek} />
+                <GMAP dayOfWeek={this.state.dayOfWeek} gymsArray={gymsArray}/>
                 <div className="gym-spots">
                     <DisplayGyms dayOfWeek={this.state.dayOfWeek} />
                 </div>
