8ff673b791fd0791d97fc3efeb25aa5d4c992f28
Joseph Jesse <jjesse@pdx.edu>
2020-03-11 12:14:49 -0700

    Unit tests to the header component

---
+++ src/tests/Header.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Header from "../components/Header";
+
+describe("Header nav test suite", () => {
+  Enzyme.configure({ adapter: new Adapter() });
+
+  it("There are three links within the navbar representing each sport", async () => {
+    const links = shallow(<Header />).find("li");
+    //All of the sport links currently available
+    const sports = ["Basketball", "Volleyball", "Tennis"];
+    expect(links.length).toEqual(sports.length);
+    for (let i = 0; i < links.length; ++i) {
+      expect(links.at(i).text()).toEqual(sports[i]);
+    }
+  });
+});
