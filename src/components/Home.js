d02024443f955b41f7015ba2cfaceab9155acd49
Larry Chiem <clarry@pdx.edu>
2020-03-11 23:48:02 -0700

    The home page is now responsive and images sizes are different.
    Signed-off-by: Larry Chiem <clarry@pdx.edu>

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
 import "./css/home.css";
-import portal1 from "./images/volleyballPortal.png";
-import portal2 from "./images/basketballPortal.png";
-import portal3 from "./images/tennisPortal.png";
-import portal4 from "./images/soccerPortal.png";
+import volleyballPortal from "./images/volleyballPortal.png";
+import basketballPortal from "./images/basketballPortal.png";
+import tennisPortal from "./images/tennisPortal.png";
 import Footer from "./Footer";
 import { Link } from "react-router-dom";
 
@@ -11,31 +10,17 @@ export default class Home extends React.Component {
   render() {
     return (
       <div className="home">
-        <br />
         <h1>Sport Spots</h1>
         <div className="container">
-          <div className="box">
-            <Link to="/Volleyball">
-              <img src={portal1} alt="volleyball-portal" />
-              <br /><br />
-            </Link>
-          </div>
-          <div className="box">
-            <Link to="/Basketball">
-              <img src={portal2} alt="basketball-portal" />
-              <br /><br />
-            </Link>
-          </div>
-          <div className="box">
-            <Link to="/Tennis">
-              <img src={portal3} alt="tennis-portal" />
-              <br /><br />
-            </Link>
-          </div>
-          <div className="box">
-            <img src={portal4} alt="soccer-portal" />
-            <br /><br />
-          </div>
+          <Link to="/Basketball">
+            <img src={basketballPortal} alt="basketball-portal" />
+          </Link>
+          <Link to="/Volleyball">
+            <img src={volleyballPortal} alt="volleyball-portal" />
+          </Link>
+          <Link to="/Tennis">
+            <img src={tennisPortal} alt="tennis-portal" />
+          </Link>
         </div>
         <Footer />
       </div>
